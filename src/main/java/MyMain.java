8bb1c783f4c44c0cc16cc45c15750986d6a7f77f
Reevak05 <kaveergera@gmail.com>
2020-09-01 17:20:18 +0000

    change to favorite number program

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your favorite number? ");
+        int num = Integer.parseInt(scan.next());
+        System.out.println("Cool! My favorite number is "+(num*2)+'.');
+        scan.close();
     }
 }
